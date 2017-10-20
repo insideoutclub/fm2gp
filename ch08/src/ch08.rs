7544c4b8b3109e604417c72ca246db64b10dfbcd
David Sanders <insideoutclub@gmail.com>
2017-10-20 08:56:52 -0700

    Added traits for Semiring

--- ch08/src/ch08.rs
+++ ch08/src/ch08.rs
@@ -22,22 +22,75 @@
 extern crate num_traits;
 extern crate std;
 
+pub trait Regular
+where
+    Self: Clone,
+    Self: std::cmp::PartialEq,
+    Self: std::cmp::PartialOrd,
+{
+}
+
+pub trait AdditiveSemigroup
+where
+    Self: Regular,
+    Self: std::ops::Add<Output = Self>,
+{
+}
+
+pub trait AdditiveMonoid
+where
+    Self: AdditiveSemigroup,
+    Self: num_traits::Zero,
+{
+}
+
+pub trait MultiplicativeSemigroup
+where
+    Self: Regular,
+    Self: std::ops::Mul<Output = Self>,
+{
+}
+
+pub trait MultiplicativeMonoid
+where
+    Self: MultiplicativeSemigroup,
+    Self: num_traits::One,
+{
+}
+
+pub trait Semiring
+where
+    Self: AdditiveMonoid,
+    Self: MultiplicativeMonoid,
+{
+}
+
+macro_rules! arithmetic_types {
+    ($($t:ty)*) => ($(
+        impl Regular for $t {}
+        impl AdditiveSemigroup for $t {}
+        impl AdditiveMonoid for $t {}
+        impl MultiplicativeSemigroup for $t {}
+        impl MultiplicativeMonoid for $t {}
+        impl Semiring for $t {}
+    )*)
+}
+
+arithmetic_types!(i8 i16 i32 i64 isize u8 u16 u32 u64 usize f32 f64);
+
 // Section 8.1
 
-pub fn polynomial_value<I, R>(first: I, x: &R) -> R
+pub fn polynomial_value<I, R: Semiring>(first: I, x: &R) -> R
 where
     I: IntoIterator<Item = R>,
-    R: num_traits::Zero,
-    R: for<'a> std::ops::Mul<&'a R, Output = R>,
-    R: for<'a> std::ops::Add<&'a R, Output = R>,
 {
     let mut iter = first.into_iter();
     match iter.next() {
         None => num_traits::zero(),
         Some(mut sum) => {
             for first in iter {
-                sum = sum * x;
-                sum = sum + &first;
+                sum = sum * x.clone();
+                sum = sum + first;
             }
             sum
         }
