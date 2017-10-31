335b62b4b7ac511f31bb4f76ec02b816e1555662
David Sanders <insideoutclub@gmail.com>
2017-10-31 10:32:02 -0700

    Adding traits

--- ch10/src/ch10.rs
+++ ch10/src/ch10.rs
@@ -19,16 +19,163 @@
 // ch10.rs -- Functions from Chapter 10 of fM2GP.
 // -------------------------------------------------------------------
 
-pub trait InputIterator
-where
-    Self: Iterator,
-{
+extern crate std;
+
+#[derive(PartialEq, Debug, Clone)]
+enum State {
+    First,
+    Last,
 }
 
-impl<T> InputIterator for T
+pub struct MyIterator<I>
 where
-    T: Iterator,
+    I: Iterator,
 {
+    state: State,
+    value: Option<I::Item>,
+    x: I,
+}
+
+impl<I> PartialEq for MyIterator<I>
+where
+    I: Iterator,
+{
+    fn eq(&self, _: &Self) -> bool {
+        self.state == State::Last
+    }
+}
+
+impl<I> InputIterator for MyIterator<I>
+where
+    I: Iterator,
+    I::Item: Clone,
+{
+    type ValueType = I::Item;
+    type DifferenceType = isize;
+    fn successor(&mut self) {
+        self.value = self.x.next();
+        if self.value.is_none() {
+            self.state = State::Last;
+        }
+    }
+    fn source(&self) -> Self::ValueType {
+        self.value.clone().unwrap()
+    }
+}
+
+/*
+#[derive(Clone, PartialEq)]
+pub struct Wrapper<I>
+where
+    I: ExactSizeIterator,
+    I: PartialEq,
+{
+    x: I,
+}
+
+impl<I> PartialEq for Wrapper<I>
+where
+    I: ExactSizeIterator,
+{
+    fn eq(&self, other: &Self) -> bool
+    {
+        self.x == other.x
+    }
+}
+
+impl<I> InputIterator for Wrapper<I>
+where
+    I: ExactSizeIterator,
+{
+    type ValueType = I::Item;
+    type DifferenceType = isize;
+    fn successor(&mut self) {
+        self.value = self.x.next();
+        if self.value.is_none() {
+            self.state = State::Last;
+        }
+    }
+    fn source(&self) -> Self::ValueType {
+        self.value.clone().unwrap()
+    }
+}
+
+impl<I> Clone for Wrapper<I>
+where
+    I: ExactSizeIterator,
+{
+    fn clone(&self) -> Self
+    {}
+}
+
+impl<I> Iterator for Wrapper<I>
+where
+    I: ExactSizeIterator,
+{
+    type Item = I::Item;
+    fn next(&mut self) -> Option<Self::Item>
+    {
+        self.next()
+    }
+}
+
+impl<I> ExactSizeIterator for Wrapper<I>
+where
+    I: ExactSizeIterator
+{
+}
+*/
+
+impl<I> Clone for MyIterator<I>
+where
+    I: Iterator,
+    I::Item: Clone,
+    I: Clone,
+{
+    fn clone(&self) -> Self {
+        Self {
+            state: self.state.clone(),
+            value: self.value.clone(),
+            x: self.x.clone(),
+        }
+    }
+}
+
+pub fn begin<I>(mut x: I) -> MyIterator<I>
+where
+    I: Iterator,
+{
+    let value = x.next();
+    MyIterator {
+        state: if value.is_some() {
+            State::First
+        } else {
+            State::Last
+        },
+        value,
+        x,
+    }
+}
+
+pub fn end<I>(x: I) -> MyIterator<I>
+where
+    I: Iterator,
+{
+    MyIterator {
+        state: State::Last,
+        value: None,
+        x,
+    }
+}
+
+pub trait InputIterator
+where
+    Self: std::cmp::PartialEq,
+{
+    type ValueType;
+    type DifferenceType;
+    fn successor(&mut self);
+    fn source(&self) -> Self::ValueType;
 }
 
 pub trait ForwardIterator
@@ -42,6 +189,12 @@ impl<T> ForwardIterator for T
 where
     T: InputIterator,
     T: Clone,
+{
+}
+
+pub trait RandomAccessIterator
+where
+    Self: ForwardIterator,
 {
 }
 
@@ -63,20 +216,27 @@ pub mod fmgp {
 
     type DifferenceType = usize;
 
-    pub fn distance_input<I>(f: I) -> DifferenceType
+    pub fn distance_input<I>(mut f: I, l: &I) -> DifferenceType
     where
         I: ::InputIterator,
     {
+        // precondition: valid_range(f, l)
         let mut n = 0;
-        for _ in f {
+        while &f != l {
+            f.successor();
             n += 1;
         }
         n
     }
 
-    pub fn distance_random_access<T>(f: &[T]) -> DifferenceType {
+    /*
+    pub fn distance_random_access<I>(f: I) -> DifferenceType
+    where
+        I: ::RandomAccessIterator,
+    {
         f.len()
     }
+    */
 
     // Section 10.6
 
@@ -86,46 +246,39 @@ pub mod fmgp {
     {
         while n != 0 {
             n -= 1;
-            x.next();
+            x.successor();
         }
     }
 
+    /*
     pub fn advance_random_access<T>(x: &[T], n: DifferenceType) -> &[T] {
         &x[n..]
     }
+    */
 
     // Section 10.7
 
-    pub fn find_if<I, P>(f: &mut I, mut p: P) -> Option<I::Item>
+    pub fn find_if<I, P>(mut f: I, l: &I, mut p: P) -> I
     where
         I: ::InputIterator,
-        P: ::Predicate<I::Item>,
+        P: ::Predicate<I::ValueType>,
     {
-        for x in f.by_ref() {
-            if p(&x) {
-                return Some(x);
-            }
+        while &f != l && !p(&f.source()) {
+            f.successor();
         }
-        None
+        f
     }
 
-    pub fn find_if_n<I, P>(
-        f: &mut I,
-        mut n: DifferenceType,
-        mut p: P,
-    ) -> (Option<I::Item>, DifferenceType)
+    pub fn find_if_n<I, P>(mut f: I, mut n: DifferenceType, mut p: P) -> (I, DifferenceType)
     where
         I: ::InputIterator,
-        P: ::Predicate<I::Item>,
+        P: ::Predicate<I::ValueType>,
     {
-        while n != 0 {
-            let x = f.next().unwrap();
-            if p(&x) {
-                return (Some(x), n);
-            }
+        while n != 0 && !p(&f.source()) {
+            f.successor();
             n -= 1;
         }
-        (None, n)
+        (f, n)
     }
 
     // Section 10.8
@@ -133,15 +286,16 @@ pub mod fmgp {
     pub fn partition_point_n<I, P>(mut f: I, mut n: DifferenceType, mut p: P) -> I
     where
         I: ::ForwardIterator,
-        P: ::Predicate<I::Item>,
+        P: ::Predicate<I::ValueType>,
     {
         while n != 0 {
             let mut middle = f.clone();
             let half = n >> 1;
             advance_input(&mut middle, half);
-            if !p(&middle.next().unwrap()) {
+            if !p(&middle.source()) {
                 n = half;
             } else {
+                middle.successor();
                 f = middle;
                 n -= half + 1;
             }
@@ -149,27 +303,27 @@ pub mod fmgp {
         f
     }
 
-    pub fn partition_point<I, P>(f: I, p: P) -> I
+    pub fn partition_point<I, P>(f: I, l: &I, p: P) -> I
     where
         I: ::ForwardIterator,
-        P: ::Predicate<I::Item>,
+        P: ::Predicate<I::ValueType>,
     {
-        partition_point_n(f.clone(), distance_input(f), p)
+        partition_point_n(f.clone(), distance_input(f, l), p)
     }
 
-    pub fn lower_bound<I>(f: I, a: &I::Item) -> I
+    pub fn lower_bound<I>(f: I, l: &I, a: &I::ValueType) -> I
     where
         I: ::ForwardIterator,
-        I::Item: ::std::cmp::PartialOrd,
+        I::ValueType: ::std::cmp::PartialOrd,
     {
-        partition_point(f, |x: &I::Item| x < a)
+        partition_point(f, l, |x: &I::ValueType| x < a)
     }
 
-    pub fn upper_bound<I>(f: I, a: &I::Item) -> I
+    pub fn upper_bound<I>(f: I, l: &I, a: &I::ValueType) -> I
     where
         I: ::ForwardIterator,
-        I::Item: ::std::cmp::PartialOrd,
+        I::ValueType: ::std::cmp::PartialOrd,
     {
-        partition_point(f, |x: &I::Item| x <= a)
+        partition_point(f, l, |x: &I::ValueType| x <= a)
     }
 } // namespace fmgp
--- ch10/src/main.rs
+++ ch10/src/main.rs
@@ -41,81 +41,103 @@ fn main() {
     println!("Initial sequence:  1 2 3 4 5");
     println!("vector v and list l initialized to this sequence.");
 
-    let mut x1 = &v[..];
-    let y1 = &v[v.len()..];
-    let mut x2 = l.iter();
+    let x1 = begin(v.iter());
+    let y1 = end(v.iter());
+    let mut x2 = begin(l.iter());
+    let y2 = end(l.iter());
     println!("Setting x1 = begin(v); y1 = end(v)");
     println!("Setting x2 = begin(l); y2 = end(l)");
 
+    /*
     println!(
         "fmgp::distance(x1, y1) is {}",
-        fmgp::distance_random_access(x1)
+        fmgp::distance_random_access(Wrapper {x: v.iter()})
     );
-    println!("fmgp::distance(x2, y2) is {}", fmgp::distance_input(x2));
+    */
+    println!(
+        "fmgp::distance(x2, y2) is {}",
+        fmgp::distance_input(x2, &y2)
+    );
+    /*
     x1 = fmgp::advance_random_access(x1, 3);
     print!("After advance(x1, 3): ");
     println!("(x1 == y1) is {}", if x1 == y1 { 1 } else { 0 });
     x2 = l.iter();
+    */
+    x2 = begin(l.iter());
     fmgp::advance_input(&mut x2, 3);
     print!("After advance(x2, 3): ");
-    println!("(x2 == y2) is {}", if x2.next().is_none() { 1 } else { 0 });
-    println!();
+    println!("(x2 == y2) is {}\n", if x2 == y2 { 1 } else { 0 });
 
+    /*
     x1 = &v[..];
-    x2 = l.iter();
+    */
+    x2 = begin(l.iter());
     println!("Setting x1 = begin(v); y1 = end(v)");
     println!("Setting x2 = begin(l); y2 = end(l)");
 
+    /*
     println!(
         "*fmgp::find_if(x1, y1, equal_3) is {}",
         fmgp::find_if(&mut x1.iter(), equal_3).unwrap()
     );
+    */
     println!(
         "*fmgp::find_if(x2, y2, equal_3) is {}",
-        fmgp::find_if(&mut x2, equal_3).unwrap()
+        fmgp::find_if(x2, &y2, equal_3).source()
     );
+    /*
     println!(
         "*fmgp::find_if_n(x1, 5, equal_3).first is {}",
         fmgp::find_if_n(&mut x1.iter(), 3, equal_3).0.unwrap()
     );
+    */
     println!(
         "*fmgp::find_if_n(x2, 5, equal_3).first is {}",
-        fmgp::find_if_n(&mut l.iter(), 3, equal_3).0.unwrap()
+        fmgp::find_if_n(begin(l.iter()), 3, equal_3).0.source()
     );
 
+    /*
     println!(
         "*fmgp::partition_point(x1, y1, less_3) is {}",
         fmgp::partition_point(x1.iter(), less_3).next().unwrap()
     );
+    */
     println!(
         "*fmgp::partition_point(x2, y2, less_3) is {}",
-        fmgp::partition_point(l.iter(), less_3).next().unwrap()
+        fmgp::partition_point(begin(l.iter()), &y2, less_3).source()
     );
+    /*
     println!(
         "*fmgp::partition_point_n(x1, 5, less_3) is {}",
         fmgp::partition_point_n(x1.iter(), 3, less_3)
             .next()
             .unwrap()
     );
+    */
     println!(
         "*fmgp::partition_point_n(x2, 5, less_3) is {}",
-        fmgp::partition_point_n(l.iter(), 3, less_3).next().unwrap()
+        fmgp::partition_point_n(begin(l.iter()), 3, less_3).source()
     );
+    /*
     println!(
         "*fmgp::upper_bound(x1, y1, 2) is {}",
         fmgp::upper_bound(x1.iter(), &&2).next().unwrap()
     );
+    */
     println!(
         "*fmgp::upper_bound(x2, y2, 2) is {}",
-        fmgp::upper_bound(l.iter(), &&2).next().unwrap()
+        fmgp::upper_bound(begin(l.iter()), &y2, &&2).source()
     );
 
+    /*
     println!(
         "*fmgp::lower_bound(x1, y1, 2) is {}",
         fmgp::lower_bound(x1.iter(), &&2).next().unwrap()
     );
+    */
     println!(
         "*fmgp::lower_bound(x2, y2, 2) is {}",
-        fmgp::lower_bound(l.iter(), &&2).next().unwrap()
+        fmgp::lower_bound(begin(l.iter()), &y2, &&2).source()
     );
 }
